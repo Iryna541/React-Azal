0d1c012569cff7d8f3559bb07afee8a47e7c901b
Naimul Haque <naimul@liberate-labs.com>
2024-02-23 19:21:46 +0600

    fix: defeault tab

--- src/pages/askq/analytics.tsx
+++ src/pages/askq/analytics.tsx
@@ -16,6 +16,7 @@ import useStoreRanking from "~/modules/bk-store-ranking/api/useStoreRanking";
 import { StoreRankingTable } from "~/modules/bk-store-ranking/StoreRankingTable";
 import { ChatInterface } from "~/modules/bk-chat/ChatInterface";
 import { useBusterIFrame } from "~/modules/askq/api/useBusterIFrame";
+import { ProtectedRoute } from "~/modules/auth/components/ProtectedRoute";
 
 export default function AnalyticsPage() {
   const { data } = useStoreRanking();
@@ -33,78 +34,80 @@ export default function AnalyticsPage() {
     });
 
   return (
-    <Layout>
-      <Title order={3}>Analytics</Title>
-      <Box m={-20} py={20}>
-        {busterIframeData && (
-          <IframeResizer
-            width="100%"
-            height={700}
-            style={{
-              border: 0,
-              width: "1px",
-              minWidth: "100%",
-              overflow: "hidden",
-            }}
-            src={busterIframeData.iframe}
-          ></IframeResizer>
-        )}
-        {isLoading && (
-          <Center h={700}>
-            <Loader size="xl" />
-          </Center>
-        )}
-      </Box>
-
-      <Tabs variant="pills" radius="xs" defaultValue="gallery">
-        <Tabs.List mb="lg">
-          <Tabs.Tab value="store">Store</Tabs.Tab>
-          <Tabs.Tab value="manager">Manager</Tabs.Tab>
-        </Tabs.List>
-        <Tabs.Panel value="store">
-          <Stack gap="xl">
-            <SimpleGrid cols={2}>
-              <ManagerRankingTable
-                title="Top 5 Best Store Managers"
-                data={topManagersData}
-              />
-              <ManagerRankingTable
-                title="Top 5 Worst Store Managers"
-                data={topManagersData}
-              />
-            </SimpleGrid>
-            <Box
+    <ProtectedRoute>
+      <Layout>
+        <Title order={3}>Analytics</Title>
+        <Box m={-20} py={20}>
+          {busterIframeData && (
+            <IframeResizer
+              width="100%"
+              height={700}
               style={{
-                border: "1px solid hsl(var(--border))",
-                borderRadius: 8,
+                border: 0,
+                width: "1px",
+                minWidth: "100%",
+                overflow: "hidden",
               }}
-            >
-              <Box px="lg" py="md">
-                <Title order={5} fw={500} fz={16}>
-                  Store Leaderboard
-                </Title>
-                <Title
-                  component="p"
-                  order={6}
-                  fz={14}
-                  fw={500}
-                  size="sm"
-                  lh={1.5}
-                >
-                  Which locations are doing better?
-                </Title>
+              src={busterIframeData.iframe}
+            ></IframeResizer>
+          )}
+          {isLoading && (
+            <Center h={700}>
+              <Loader size="lg" type="dots" />
+            </Center>
+          )}
+        </Box>
+
+        <Tabs variant="pills" radius="xs" defaultValue="store">
+          <Tabs.List mb="lg">
+            <Tabs.Tab value="store">Store</Tabs.Tab>
+            <Tabs.Tab value="manager">Manager</Tabs.Tab>
+          </Tabs.List>
+          <Tabs.Panel value="store">
+            <Stack gap="xl">
+              <SimpleGrid cols={2}>
+                <ManagerRankingTable
+                  title="Top 5 Best Store Managers"
+                  data={topManagersData}
+                />
+                <ManagerRankingTable
+                  title="Top 5 Worst Store Managers"
+                  data={topManagersData}
+                />
+              </SimpleGrid>
+              <Box
+                style={{
+                  border: "1px solid hsl(var(--border))",
+                  borderRadius: 8,
+                }}
+              >
+                <Box px="lg" py="md">
+                  <Title order={5} fw={500} fz={16}>
+                    Store Leaderboard
+                  </Title>
+                  <Title
+                    component="p"
+                    order={6}
+                    fz={14}
+                    fw={500}
+                    size="sm"
+                    lh={1.5}
+                  >
+                    Which locations are doing better?
+                  </Title>
+                </Box>
+                <Divider />
+                {data && <StoreRankingTable data={data} />}
               </Box>
-              <Divider />
-              {data && <StoreRankingTable data={data} />}
-            </Box>
-          </Stack>
-        </Tabs.Panel>
-        <Tabs.Panel value="manager">manager</Tabs.Panel>
-      </Tabs>
-      <Box mt="xl">
-        <ChatInterface />
-      </Box>
-    </Layout>
+            </Stack>
+          </Tabs.Panel>
+          <Tabs.Panel value="manager">manager</Tabs.Panel>
+        </Tabs>
+        <Box mt="xl">
+          <ChatInterface />
+        </Box>
+      </Layout>
+    </ProtectedRoute>
   );
 }
 
