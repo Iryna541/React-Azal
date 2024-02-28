2e373c151e6f79088b05d642a3eda1a6d1b4e7bc
Naimul Haque <naimul@liberate-labs.com>
2024-02-28 21:03:25 +0600

    fix: ts error

--- src/pages/bk-chat.tsx
+++ src/pages/bk-chat.tsx
@@ -1,10 +1,15 @@
 import { Container } from "@mantine/core";
+import { BkLogo } from "~/modules/bk/bk-chat/BkLogo";
 import { BkChatInterface } from "~/modules/bk/bk-chat/ChatInterface";
 
 export function BkChatPage() {
   return (
     <Container size="100%">
-      <BkChatInterface />
+      <BkChatInterface
+        type="bk"
+        logo={<BkLogo height={38} width={38} />}
+        title="BK University Chatbot"
+      />
     </Container>
   );
 }
