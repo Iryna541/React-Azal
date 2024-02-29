4e431e44c8cef9aa6575979a017a4e4d3c54e017
Iryna541 <irynaivolkova@gmail.com>
2024-02-29 23:09:35 +0600

    fix: add place icons

--- src/modules/restaurant365/store-ranking/columns.tsx
+++ src/modules/restaurant365/store-ranking/columns.tsx
@@ -2,11 +2,33 @@ import { ColumnDef } from "@tanstack/react-table";
 import { StoreInsights } from "./api/useR365StoreRanking"; // Adjusted import for the new hook
 import { ActionIcon, Badge, Text } from "@mantine/core";
 import { IconInfoCircleFilled } from "@tabler/icons-react";
+import {
+  IconFifthPlace,
+  IconFirstPlace,
+  IconFourthPlace,
+  IconSecondPlace,
+  IconThirdPlace,
+} from "~/assets";
 
 export const columns: ColumnDef<StoreInsights>[] = [
   {
     accessorKey: "total_net_sales_rank",
     header: "Position",
+    cell: ({ getValue }) => {
+      const value = getValue() as string;
+      if (value === "1") {
+        return <IconFirstPlace height={28} width={28} />;
+      } else if (value === "2") {
+        return <IconSecondPlace height={28} width={28} />;
+      } else if (value === "3") {
+        return <IconThirdPlace height={28} width={28} />;
+      } else if (value === "4") {
+        return <IconFourthPlace height={28} width={28} />;
+      } else if (value === "5") {
+        return <IconFifthPlace height={28} width={28} />;
+      }
+      return value;
+    },
   },
   {
     accessorKey: "store_id",
