88ed8239d3cc612c6eb7bce85cb8c64819dc4dca
Iryna541 <irynaivolkova@gmail.com>
2024-03-08 21:35:17 +0600

    feat(dunkin): manager table plan implemented

---
+++ src/modules/dunkin/dunkin-manager-plan/api/useDunkinManagerPlan.ts
@@ -0,0 +1,30 @@
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import axios from "axios";
+
+export type ManagerPlanRow = {
+  store_id: string;
+  subRows: Array<{
+    shift: string;
+    managers_plan: string;
+  }>;
+};
+
+export type ManagerPlanResponse = Array<ManagerPlanRow>;
+
+export async function getDunkinManagerPlan(): Promise<ManagerPlanResponse> {
+  return axios
+    .get("https://azalio-bk-api.cosmos.staging.delineate.pro/dunkin-manager-plan") // Adjusted URL for manager plans
+    .then((res) => res.data);
+}
+
+export type UseDunkinManagerPlanOptions = {
+  config?: UseQueryOptions<ManagerPlanResponse>;
+};
+
+export function useDunkinManagerPlan({ config }: UseDunkinManagerPlanOptions = {}) {
+  return useQuery({
+    queryKey: ["bk-manager-plan"],
+    queryFn: getDunkinManagerPlan,
+    ...config,
+  });
+}
