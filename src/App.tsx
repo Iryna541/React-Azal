2e631bba1ec7715ec4a9bd14e9331fce4ffbcc9c
Iryna541 <irynaivolkova@gmail.com>
2024-03-12 12:00:41 +0600

    feat(bk): add charts

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import setupLogRocketReact from "logrocket-react";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/notifications/styles.css";
+import "@mantine/charts/styles.css";
 import "./styles/global.css";
 
 import { getRoutes } from "./routes";
