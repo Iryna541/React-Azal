6d8b91ce8007c2be6b8b797a890257ecb53df8d1
JAHED <macbookpro@MacBooks-MacBook-Pro.local>
2024-05-10 00:27:43 +0600

    Russ lto training data added

---
+++ src/modules/bk/lto-training-report/RussLtoTrainingReportTable.tsx
@@ -0,0 +1,147 @@
+import { useState } from "react";
+import { storeWiseColumns } from "./columns";
+import {
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { Box, Table } from "@mantine/core";
+
+const RussLtoTrainingReportTable = ({
+  data,
+  columns,
+  colVisibility,
+}: {
+  data: any;
+  columns: any;
+  colVisibility: VisibilityState;
+}) => {
+  const PAGE_SIZE = 10;
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnVisibility, setColumnVisibility] =
+    useState<VisibilityState>(colVisibility);
+
+  const table = useReactTable({
+    data,
+    columns,
+    autoResetPageIndex: false,
+    getCoreRowModel: getCoreRowModel(),
+    // pagination
+    getPaginationRowModel: getPaginationRowModel(),
+    // column filters
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    // column visibility
+    onColumnVisibilityChange: setColumnVisibility,
+    // sorting
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+
+    // initial state
+    initialState: {
+      pagination: {
+        pageSize: PAGE_SIZE,
+      },
+    },
+    // state
+    state: {
+      columnFilters,
+      columnVisibility,
+      sorting,
+    },
+  });
+
+  return (
+    <Box
+      style={{ border: "1px solid hsl(var(--border))", borderRadius: "8px" }}
+    >
+      <Table horizontalSpacing="lg" withColumnBorders verticalSpacing="sm">
+        <Table.Thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <Table.Tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <Table.Th
+                    key={header.id}
+                    style={{
+                      width: header.column.getSize(),
+                      minWidth: header.column.getSize(),
+                      maxWidth: header.column.getSize(),
+                    }}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </Table.Th>
+                );
+              })}
+            </Table.Tr>
+          ))}
+        </Table.Thead>
+        <Table.Tbody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => {
+              return (
+                <>
+                  <Table.Tr
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                    onClick={() => {
+                      row.getIsExpanded()
+                        ? row.toggleExpanded(false)
+                        : row.toggleExpanded(true);
+                    }}
+                    style={{ cursor: "pointer" }}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <Table.Td
+                        fw={500}
+                        c="hsl(var(--foreground) / 0.8)"
+                        key={cell.id}
+                        style={{
+                          width: cell.column.getSize(),
+                          minWidth: cell.column.getSize(),
+                          maxWidth: cell.column.getSize(),
+                        }}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </Table.Td>
+                    ))}
+                  </Table.Tr>
+                </>
+              );
+            })
+          ) : (
+            <Table.Tr>
+              <Table.Td
+                colSpan={storeWiseColumns.length}
+                className="h-24 text-center"
+              >
+                No results.
+              </Table.Td>
+            </Table.Tr>
+          )}
+        </Table.Tbody>
+      </Table>
+    </Box>
+  );
+};
+
+export default RussLtoTrainingReportTable;
---
+++ src/modules/bk/lto-training-report/columns.tsx
@@ -0,0 +1,304 @@
+import { Badge, Grid, Loader, Stack, Text } from "@mantine/core";
+import { modals } from "@mantine/modals";
+import { ColumnDef } from "@tanstack/react-table";
+import { ReactNode } from "react";
+import { useRussLtoTrainingReportDetail } from "./api/useRussLtoTrainings";
+
+export const storeWiseColumns: ColumnDef<any>[] = [
+  {
+    accessorKey: "store_id",
+    header: "Store ID",
+    cell: (cell) => (
+      <Badge
+        size="md"
+        h={32}
+        w={"100%"}
+        c="hsl(var(--foreground) / 0.8)"
+        bg="hsl(var(--secondary))"
+        fw={600}
+        fz={14}
+        style={{ borderRadius: 4, cursor: "pointer" }}
+        onClick={() => {
+          modals.open({
+            title: `Store Details ${cell.getValue()}`,
+            size: "xl",
+            children: <ModalContent storeId={cell.getValue() as string} />,
+          });
+        }}
+      >
+        {cell.getValue() as string}
+      </Badge>
+    ),
+    // size: 80,
+  },
+  {
+    accessorKey: "total_enrolled",
+    header: "Total Enrolled",
+    // size: 120,
+  },
+  {
+    accessorKey: "total_completed",
+    header: "Total Completed",
+    // size: 120,
+  },
+  {
+    accessorKey: "total_not_completed",
+    header: "Total Not Completed",
+    // size: 120,
+  },
+  {
+    accessorKey: "completion_percentage",
+    header: "Completion (%)",
+    // size: 120,
+  },
+];
+
+export const dtlWiseColumns: ColumnDef<any>[] = [
+  {
+    accessorKey: "manager_id",
+    header: "Manager ID",
+    size: 80,
+  },
+  {
+    accessorKey: "manager_name",
+    header: "DTL",
+    cell: (cell) => (
+      <Badge
+        size="md"
+        h={32}
+        w={"100%"}
+        c="hsl(var(--foreground) / 0.8)"
+        bg="hsl(var(--secondary))"
+        fw={600}
+        fz={14}
+        style={{ borderRadius: 4, cursor: "pointer" }}
+        onClick={() => {
+          modals.open({
+            title: `DTL Details (${cell.getValue() as string})`,
+            size: "xl",
+            children: (
+              <StoreWiseModalContent
+                managerId={cell.row.original.manager_id as string}
+              />
+            ),
+          });
+        }}
+      >
+        {cell.getValue() as string}
+      </Badge>
+    ),
+    // size: 120,
+  },
+  {
+    accessorKey: "total_enrolled",
+    header: "Total Enrolled",
+    // size: 120,
+  },
+  {
+    accessorKey: "total_completed",
+    header: "Total Completed",
+    // size: 120,
+  },
+  {
+    accessorKey: "total_not_completed",
+    header: "Total Not Completed",
+    // size: 120,
+  },
+  {
+    accessorKey: "completion_percentage",
+    header: "Completion (%)",
+    // size: 120,
+  },
+];
+
+export function ModalContent({ storeId }: { storeId: string }) {
+  const { data: data, isFetching } = useRussLtoTrainingReportDetail({
+    params: { storeId },
+  });
+  function HeaderCell({
+    title,
+    textAlign = "center",
+  }: {
+    title: string;
+    textAlign?: any;
+  }): ReactNode {
+    return (
+      <Text size="sm" fw={"700"} ta={textAlign}>
+        {title}
+      </Text>
+    );
+  }
+
+  return (
+    <Stack>
+      <Grid grow>
+        <Grid.Col span={2}>
+          <HeaderCell title="DTL" textAlign={"left"} />
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <HeaderCell title="T. Enrolled" />
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <HeaderCell title="T. Completed" />
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <HeaderCell title="T. Not Completed" />
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <HeaderCell title="Completion (%)" />
+        </Grid.Col>
+      </Grid>
+      {data?.map((item: any) => (
+        <Grid grow>
+          <Grid.Col span={2}>
+            {!isFetching ? (
+              <Text>{item.name}</Text>
+            ) : (
+              <Text>
+                <Loader />
+              </Text>
+            )}
+          </Grid.Col>
+          <Grid.Col span={1}>
+            {!isFetching ? (
+              <Text style={{ textAlign: "center" }}>{item.total_enrolled}</Text>
+            ) : (
+              <Text style={{ textAlign: "center" }}>
+                <Loader />
+              </Text>
+            )}
+          </Grid.Col>
+          <Grid.Col span={1}>
+            {!isFetching ? (
+              <Text style={{ textAlign: "center" }}>
+                {item.total_completed}
+              </Text>
+            ) : (
+              <Text style={{ textAlign: "center" }}>
+                <Loader />
+              </Text>
+            )}
+          </Grid.Col>
+          <Grid.Col span={1}>
+            {!isFetching ? (
+              <Text style={{ textAlign: "center" }}>
+                {item.total_not_completed}
+              </Text>
+            ) : (
+              <Text style={{ textAlign: "center" }}>
+                <Loader />
+              </Text>
+            )}
+          </Grid.Col>
+          <Grid.Col span={1}>
+            {!isFetching ? (
+              <Text style={{ textAlign: "center" }}>
+                {item.completion_percentage}
+              </Text>
+            ) : (
+              <Text style={{ textAlign: "center" }}>
+                <Loader />
+              </Text>
+            )}
+          </Grid.Col>
+        </Grid>
+      ))}
+    </Stack>
+  );
+}
+
+export function StoreWiseModalContent({ managerId }: { managerId: string }) {
+  const { data: data, isFetching } = useRussLtoTrainingReportDetail({
+    params: { managerId },
+  });
+  function HeaderCell({
+    title,
+    textAlign = "center",
+  }: {
+    title: string;
+    textAlign?: any;
+  }): ReactNode {
+    return (
+      <Text size="sm" fw={"700"} ta={textAlign}>
+        {title}
+      </Text>
+    );
+  }
+
+  return (
+    <Stack>
+      <Grid grow>
+        <Grid.Col span={1}>
+          <HeaderCell title="Store ID" />
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <HeaderCell title="T. Enrolled" />
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <HeaderCell title="T. Completed" />
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <HeaderCell title="T. Not Completed" />
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <HeaderCell title="Completion (%)" />
+        </Grid.Col>
+      </Grid>
+      {data?.map((item: any) => (
+        <Grid grow>
+          <Grid.Col span={1}>
+            {!isFetching ? (
+              <Text style={{ textAlign: "center" }}>{item.store_id}</Text>
+            ) : (
+              <Text style={{ textAlign: "center" }}>
+                <Loader />
+              </Text>
+            )}
+          </Grid.Col>
+          <Grid.Col span={1}>
+            {!isFetching ? (
+              <Text style={{ textAlign: "center" }}>{item.total_enrolled}</Text>
+            ) : (
+              <Text style={{ textAlign: "center" }}>
+                <Loader />
+              </Text>
+            )}
+          </Grid.Col>
+          <Grid.Col span={1}>
+            {!isFetching ? (
+              <Text style={{ textAlign: "center" }}>
+                {item.total_completed}
+              </Text>
+            ) : (
+              <Text style={{ textAlign: "center" }}>
+                <Loader />
+              </Text>
+            )}
+          </Grid.Col>
+          <Grid.Col span={1}>
+            {!isFetching ? (
+              <Text style={{ textAlign: "center" }}>
+                {item.total_not_completed}
+              </Text>
+            ) : (
+              <Text style={{ textAlign: "center" }}>
+                <Loader />
+              </Text>
+            )}
+          </Grid.Col>
+          <Grid.Col span={1}>
+            {!isFetching ? (
+              <Text style={{ textAlign: "center" }}>
+                {item.completion_percentage}
+              </Text>
+            ) : (
+              <Text style={{ textAlign: "center" }}>
+                <Loader />
+              </Text>
+            )}
+          </Grid.Col>
+        </Grid>
+      ))}
+    </Stack>
+  );
+}
