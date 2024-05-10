ae3da5e8b55e07746ff6cc2de448465609032af1
JAHED <macbookpro@MacBooks-MacBook-Pro.local>
2024-05-10 22:14:46 +0600

    feedback solved

--- src/modules/bk/lto-training-report/columns.tsx
+++ src/modules/bk/lto-training-report/columns.tsx
@@ -1,34 +1,39 @@
 import { Badge, Grid, Loader, Stack, Text } from "@mantine/core";
 import { modals } from "@mantine/modals";
 import { ColumnDef } from "@tanstack/react-table";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { useRussLtoTrainingReportDetail } from "./api/useRussLtoTrainings";
 
 export const storeWiseColumns: ColumnDef<any>[] = [
   {
     accessorKey: "store_id",
     header: "Store ID",
-    cell: (cell) => (
-      <Badge
-        size="md"
-        h={32}
-        w={"100%"}
-        c="hsl(var(--foreground) / 0.8)"
-        bg="hsl(var(--secondary))"
-        fw={600}
-        fz={14}
-        style={{ borderRadius: 4, cursor: "pointer" }}
-        onClick={() => {
-          modals.open({
-            title: `Store Details (${cell.getValue()})`,
-            size: "xl",
-            children: <ModalContent storeId={cell.getValue() as string} />,
-          });
-        }}
-      >
-        {cell.getValue() as string}
-      </Badge>
-    ),
+    cell: (cell) =>
+      cell.getValue() === "Grand Total" ? (
+        <Text ta={"center"} fw={"600"}>
+          {cell.getValue() as string}
+        </Text>
+      ) : (
+        <Badge
+          size="md"
+          h={32}
+          w={"100%"}
+          c="hsl(var(--foreground) / 0.8)"
+          bg="hsl(var(--secondary))"
+          fw={600}
+          fz={14}
+          style={{ borderRadius: 4, cursor: "pointer" }}
+          onClick={() => {
+            modals.open({
+              title: `Store Details (${cell.getValue()})`,
+              size: "xl",
+              children: <ModalContent storeId={cell.getValue() as string} />,
+            });
+          }}
+        >
+          {cell.getValue() as string}
+        </Badge>
+      ),
     // size: 80,
   },
   {
@@ -49,6 +54,9 @@ export const storeWiseColumns: ColumnDef<any>[] = [
   {
     accessorKey: "completion_percentage",
     header: "Completion (%)",
+    cell: (cell) => {
+      return `${cell.getValue()} %`;
+    },
     // size: 120,
   },
 ];
@@ -62,31 +70,36 @@ export const dtlWiseColumns: ColumnDef<any>[] = [
   {
     accessorKey: "manager_name",
     header: "DTL",
-    cell: (cell) => (
-      <Badge
-        size="md"
-        h={32}
-        w={"100%"}
-        c="hsl(var(--foreground) / 0.8)"
-        bg="hsl(var(--secondary))"
-        fw={600}
-        fz={14}
-        style={{ borderRadius: 4, cursor: "pointer" }}
-        onClick={() => {
-          modals.open({
-            title: `DTL Details (${cell.getValue() as string})`,
-            size: "xl",
-            children: (
-              <StoreWiseModalContent
-                managerId={cell.row.original.manager_id as string}
-              />
-            ),
-          });
-        }}
-      >
-        {cell.getValue() as string}
-      </Badge>
-    ),
+    cell: (cell) =>
+      cell.getValue() === "Grand Total" ? (
+        <Text ta={"center"} fw={"600"}>
+          {cell.getValue() as string}
+        </Text>
+      ) : (
+        <Badge
+          size="md"
+          h={32}
+          w={"100%"}
+          c="hsl(var(--foreground) / 0.8)"
+          bg="hsl(var(--secondary))"
+          fw={600}
+          fz={14}
+          style={{ borderRadius: 4, cursor: "pointer" }}
+          onClick={() => {
+            modals.open({
+              title: `DTL Details (${cell.getValue() as string})`,
+              size: "xl",
+              children: (
+                <StoreWiseModalContent
+                  managerId={cell.row.original.manager_id as string}
+                />
+              ),
+            });
+          }}
+        >
+          {cell.getValue() as string}
+        </Badge>
+      ),
     // size: 120,
   },
   {
@@ -107,6 +120,9 @@ export const dtlWiseColumns: ColumnDef<any>[] = [
   {
     accessorKey: "completion_percentage",
     header: "Completion (%)",
+    cell: (cell) => {
+      return `${cell.getValue()} %`;
+    },
     // size: 120,
   },
 ];
@@ -115,6 +131,35 @@ export function ModalContent({ storeId }: { storeId: string }) {
   const { data: data, isFetching } = useRussLtoTrainingReportDetail({
     params: { storeId },
   });
+  const [dtlWiseData, setDtlWiseData] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      const tempData = data;
+      const finalRow = {
+        name: "Grand Total",
+        total_enrolled: tempData.reduce(
+          (acc: number, row: any) => acc + row.total_enrolled,
+          0
+        ),
+        total_completed: tempData.reduce(
+          (acc: number, row: any) => acc + row.total_completed,
+          0
+        ),
+        total_not_completed: tempData.reduce(
+          (acc: number, row: any) => acc + row.total_not_completed,
+          0
+        ),
+        completion_percentage: (
+          (tempData.reduce((acc: number, row: any) => acc + row.total_completed, 0) /
+            tempData.reduce((acc: number, row: any) => acc + row.total_enrolled, 0)) *
+          100
+        ).toFixed(2),
+      };
+      setDtlWiseData([...tempData, finalRow]);
+    }
+  }, [data]);
+
   function HeaderCell({
     title,
     textAlign = "center",
@@ -133,7 +178,7 @@ export function ModalContent({ storeId }: { storeId: string }) {
     <Stack>
       <Grid grow>
         <Grid.Col span={2}>
-          <HeaderCell title="DTL" textAlign={"left"} />
+          <HeaderCell title="DTL" />
         </Grid.Col>
         <Grid.Col span={1}>
           <HeaderCell title="T. Enrolled" />
@@ -148,13 +193,17 @@ export function ModalContent({ storeId }: { storeId: string }) {
           <HeaderCell title="Completion (%)" />
         </Grid.Col>
       </Grid>
-      {data?.map((item: any) => (
-        <Grid grow>
+      {dtlWiseData?.map((item: any) => (
+        <Grid
+          grow
+          bg={item.name === "Grand Total" ? "hsl(var(--secondary))" : ""}
+          py={item.name === "Grand Total" ? 10 : 0}
+        >
           <Grid.Col span={2}>
             {!isFetching ? (
-              <Text>{item.name}</Text>
+              <Text style={{ textAlign: "center" }}>{item.name}</Text>
             ) : (
-              <Text>
+              <Text style={{ textAlign: "center" }}>
                 <Loader />
               </Text>
             )}
@@ -193,7 +242,7 @@ export function ModalContent({ storeId }: { storeId: string }) {
           <Grid.Col span={1}>
             {!isFetching ? (
               <Text style={{ textAlign: "center" }}>
-                {item.completion_percentage}
+                {item.completion_percentage} %
               </Text>
             ) : (
               <Text style={{ textAlign: "center" }}>
@@ -211,6 +260,35 @@ export function StoreWiseModalContent({ managerId }: { managerId: string }) {
   const { data: data, isFetching } = useRussLtoTrainingReportDetail({
     params: { managerId },
   });
+  const [storeWiseData, setStoreWiseData] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      const tempData = data;
+      const finalRow = {
+        store_id: "Grand Total",
+        total_enrolled: tempData.reduce(
+          (acc: number, row: any) => acc + row.total_enrolled,
+          0
+        ),
+        total_completed: tempData.reduce(
+          (acc: number, row: any) => acc + row.total_completed,
+          0
+        ),
+        total_not_completed: tempData.reduce(
+          (acc: number, row: any) => acc + row.total_not_completed,
+          0
+        ),
+        completion_percentage: (
+          (tempData.reduce((acc: number, row: any) => acc + row.total_completed, 0) /
+            tempData.reduce((acc: number, row: any) => acc + row.total_enrolled, 0)) *
+          100
+        ).toFixed(2),
+      };
+      setStoreWiseData([...tempData, finalRow]);
+    }
+  }, [data]);
+
   function HeaderCell({
     title,
     textAlign = "center",
@@ -229,7 +307,7 @@ export function StoreWiseModalContent({ managerId }: { managerId: string }) {
     <Stack>
       <Grid grow>
         <Grid.Col span={1}>
-          <HeaderCell title="Store ID" textAlign={"left"} />
+          <HeaderCell title="Store ID" />
         </Grid.Col>
         <Grid.Col span={1}>
           <HeaderCell title="T. Enrolled" />
@@ -244,13 +322,17 @@ export function StoreWiseModalContent({ managerId }: { managerId: string }) {
           <HeaderCell title="Completion (%)" />
         </Grid.Col>
       </Grid>
-      {data?.map((item: any) => (
-        <Grid grow>
+      {storeWiseData?.map((item: any) => (
+        <Grid
+          grow
+          bg={item.store_id === "Grand Total" ? "hsl(var(--secondary))" : ""}
+          py={item.name === "Grand Total" ? 10 : 0}
+        >
           <Grid.Col span={1}>
             {!isFetching ? (
-              <Text>{item.store_id}</Text>
+              <Text style={{ textAlign: "center" }}>{item.store_id}</Text>
             ) : (
-              <Text>
+              <Text style={{ textAlign: "center" }}>
                 <Loader />
               </Text>
             )}
@@ -289,7 +371,7 @@ export function StoreWiseModalContent({ managerId }: { managerId: string }) {
           <Grid.Col span={1}>
             {!isFetching ? (
               <Text style={{ textAlign: "center" }}>
-                {item.completion_percentage}
+                {item.completion_percentage}%
               </Text>
             ) : (
               <Text style={{ textAlign: "center" }}>
